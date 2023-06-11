0258efe9af01cb5bddccaeceea7d2f5d26370b59
louvivien <vivien.richaud@gmail.com>
2023-06-11 13:29:22 +0200

    loading autogpt first

--- app.py
+++ app.py
@@ -21,134 +21,71 @@ def load_env():
     dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
     load_dotenv(dotenv_path)
 
+
 # Function to message a user on Instagram
-def instagrapi_message(username, password, proxy=None):
-    print("Initializing Instagram client...")
-    # Create a client object
-    client = Client()
-
-    # Set a random delay between 1 and 3 seconds after each request
-    client.delay_range = [1, 3]
-    
-    # Load session from file if it exists
-    session_file = f"{username}_session.json"
-    if os.path.exists(session_file):
-        client.load_settings(session_file)
-        print("Session loaded from file")
-    
-    # Login to Instagram
-    print("Logging in to Instagram...")
-    try:
-        client.login(username, password)
-        print('Logged in')
-    except LoginRequired:
-        print("Session expired. Logging in again...")
-        client.login(username, password)
-        print('Logged in')
-    
-    # Save session to file
-    client.dump_settings(session_file)
-    print("Session saved to file")
-    
-    # Set proxy if provided
-    if proxy:
-        print("Setting up proxy...")
-        client.set_proxy(proxy)
-        # http://<api_key>:wifi;ca;;;toronto@proxy.soax.com:9137
-        print("Proxy set up")
-
-    # Get the user id of louvivien
-    print("Getting user ID...")
-    user_id = client.user_id_from_username('louvivien')
-                                # # Follow user if not already following, you need this to send messages
-                                # result = client.user_follow(user_id)
-                                # # Check if the follow was successful
-                                # if result == True:
-                                #     print('Followed successfully')
-                                # else:
-                                #     print('Follow failed')
-    print("User found")                      
-
-    # Send a direct message
-    print("Sending message...")                      
-    message = 'Hello, this is Tom, how are you?'
-    try:
-        result = client.direct_send(message, user_ids=[user_id])
-        print('Message sent successfully')
-
-        # Get the thread_id from the result
-        thread_id = result.thread_id
-
-        # Get the thread with the user
-        thread = client.direct_thread(thread_id)
-
-        # Keep track of the last message you sent
-        last_message_id = thread.messages[0].id
-
-        # Wait for the user to reply
-        print("Waiting for reply...")
-        for _ in range(3):  # try 3 times
-            client.delay_range = [27, 30]
-            # Refresh the thread
-            thread = client.direct_thread(thread_id)
-
-            # Check for new messages
-            new_messages = [m for m in thread.messages if m.id > last_message_id]
-
-            if new_messages:
-                # Process new messages
-                for message in new_messages:
-                    print(f"Received message: {message.text}")
-
-                # Update the last message id
-                last_message_id = thread.messages[0].id
-                return message.text  # Return the text of the last received message
-            else:
-                print("No reply received yet. Waiting again...")
-                client.delay_range = [57, 60] # wait for another 60 seconds
-        print("No reply received after 3 attempts.")
-        return None
-    except ClientError as e:
-        print('Message failed')
-        print(e)
-
-
-
 class InstagramTool:
-    def __init__(self, username, password):
+    def __init__(self, username, password, proxy=None):
+        print("Initializing Instagram client...")
         self.client = Client()
         self.client.delay_range = [1, 3]
         session_file = f"{username}_session.json"
         if os.path.exists(session_file):
             self.client.load_settings(session_file)
+            print("Session loaded from file")
         try:
+            print("Logging in to Instagram...")
             self.client.login(username, password)
+            print('Logged in')
         except LoginRequired:
+            print("Session expired. Logging in again...")
             self.client.login(username, password)
+            print('Logged in')
         self.client.dump_settings(session_file)
+        print("Session saved to file")
+        if proxy:
+            print("Setting up proxy...")
+            self.client.set_proxy(proxy)
+            print("Proxy set up")
+        print("Getting user ID...")
         self.user_id = self.client.user_id_from_username('louvivien')
+        print("User found")
 
     def send_message(self, message):
-        result = self.client.direct_send(message, user_ids=[self.user_id])
-        thread_id = result.thread_id
-        thread = self.client.direct_thread(thread_id)
-        last_message_id = thread.messages[0].id
-        return last_message_id, thread_id
+        print("Sending message...")
+        try:
+            result = self.client.direct_send(message, user_ids=[self.user_id])
+            print('Message sent successfully')
+            thread_id = result.thread_id
+            thread = self.client.direct_thread(thread_id)
+            last_message_id = thread.messages[0].id
+            return last_message_id, thread_id
+        except ClientError as e:
+            print('Message failed')
+            print(e)
 
     def receive_message(self, last_message_id, thread_id):
-        self.client.delay_range = [27, 30]
-        thread = self.client.direct_thread(thread_id)
-        new_messages = [m for m in thread.messages if m.id > last_message_id]
-        if not new_messages:
-            self.client.delay_range = [57, 60]
-        return new_messages
+        print("Waiting for reply...")
+        for _ in range(3):  # try 3 times
+            self.client.delay_range = [27, 30]
+            thread = self.client.direct_thread(thread_id)
+            new_messages = [m for m in thread.messages if m.id > last_message_id]
+            if new_messages:
+                for message in new_messages:
+                    print(f"Received message: {message.text}")
+                last_message_id = thread.messages[0].id
+                return new_messages
+            else:
+                print("No reply received yet. Waiting again...")
+                self.client.delay_range = [57, 60]
+        print("No reply received after 3 attempts.")
+        return None
 
-
-
-def run_autogpt(goal, reply):
+def run_autogpt(goal, username, password):
+    print("Setting up tools for AutoGPT")
     # Set up tools for AutoGPT
     search = SerpAPIWrapper()
 
+    print("Setting up Instagram for AutoGPT")
     # Create an instance of InstagramTool
     instagram_tool = InstagramTool(username, password)
 
@@ -173,7 +110,6 @@ def run_autogpt(goal, reply):
         ),
     ]
 
-
     # Set up memory for AutoGPT
     embeddings_model = OpenAIEmbeddings()
     embedding_size = 1536
@@ -187,11 +123,12 @@ def run_autogpt(goal, reply):
         tools=tools,
         llm=ChatOpenAI(temperature=0),
         memory=vectorstore.as_retriever()
-    )
+       )
     agent.chain.verbose = True
 
     # Run AutoGPT with your goal and pass the user's reply as an argument
-    agent.run([goal, reply])
+    print("Running AutoGPT...")
+    agent.run([goal])
 
 if __name__ == "__main__":
     # Load environment variables
@@ -200,17 +137,7 @@ if __name__ == "__main__":
     password = os.getenv('INSTA_PASSWORD')
 
     # Set your goal as a natural language string
-    goal = "Send a message to an Instagram user and then use Auto-GPT to engage in a conversation based on their reply"
-    
-    # Get the reply from the Instagram user
-    reply = instagrapi_message(username, password)
-    goal2 = "User's reply :"+reply
+    goal = "Engage in a conversation with an Instagram user"
 
-    # If there is no reply after 3 attempts, tell AutoGPT to wait for one hour
-    if reply is None:
-        print("No reply received yet. Waiting for one hour before checking again...")
-        time.sleep(3600)  # wait for one hour
-        reply = instagrapi_message(username, password)
-
-    # Run AutoGPT with the goal and the reply (as a second goal)
-    run_autogpt(goal, goal2)
+    # Run AutoGPT with the goal
+    run_autogpt(goal, username, password)
