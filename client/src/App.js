740f16cd125f59d27de9b638b038168af4c09fb9
Louvivien <43605778+Louvivien@users.noreply.github.com>
2023-07-31 15:55:47 +0200

    front fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
+import React, { useState } from 'react';
+import UserContext from "./context/UserContext";
 import "./App.css";
 import Routers from "./routes/Routers";
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
+    <UserContext.Provider value={{ user, setUser }}>
+
     <div>
       <Routers />
     </div>
+    </UserContext.Provider>
+
   );
 }
 
